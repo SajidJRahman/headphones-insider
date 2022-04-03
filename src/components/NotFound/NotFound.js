7998d8ec2e4b56b5a3e6e5ec03befe2bf4047784
Sajid Rahman <sajidjrahman@icloud.com>
2022-04-04 00:54:12 +0200

    added 404 page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './NotFound.css';
 
 const NotFound = () => {
     return (
-        <div>
+        <div className='not-found text-center'>
             <h1>404</h1>
+            <h2>Hey captain! Looks like you're heading to a wrong planet!</h2>
+            <Link to='/'>
+                <button>Take Me Back To Homepage</button>
+            </Link>
         </div>
     );
 };
