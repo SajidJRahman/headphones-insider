d215fd1147bfbae12eb30a3725404849f51ccbaa
Sajid Rahman <sajidjrahman@icloud.com>
2022-04-03 23:52:52 +0200

    finished navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
+    function CustomLink({ children, to, ...props }) {
+        let resolved = useResolvedPath(to);
+        let match = useMatch({ path: resolved.pathname, end: true });
+
+        return (
+            <div>
+                <Link
+                    style={match ? { color: 'rgb(17, 150, 255)', border: '1px solid rgb(17, 150, 255)', padding: '5px 8px', margin: '11px', borderRadius: '5px' } : { color: '' }}
+                    to={to}
+                    {...props}
+                >
+                    {children}
+                </Link>
+                {match && ""}
+            </div >
+        );
+    }
+
     return (
         <nav className='navbar-links'>
-            <Link to='/home'>HOME</Link>
-            <Link to='/reviews'>REVIEWS</Link>
-            <Link to='/dashboard'>DASHBOARD</Link>
-            <Link to='/blogs'>BLOGS</Link>
-            <Link to='/about'>ABOUT</Link>
+            <CustomLink to='/home'>HOME</CustomLink>
+            <CustomLink to='/reviews'>REVIEWS</CustomLink>
+            <CustomLink to='/dashboard'>DASHBOARD</CustomLink>
+            <CustomLink to='/blogs'>BLOGS</CustomLink>
+            <CustomLink to='/about'>ABOUT</CustomLink>
         </nav>
     );
 };
